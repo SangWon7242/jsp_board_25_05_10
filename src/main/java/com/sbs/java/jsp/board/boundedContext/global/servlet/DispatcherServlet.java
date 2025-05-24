a457236ef95e005561b199a742ff871c05d82444
SangWon7242 <gotkdqja@gmail.com>
2025-05-24 12:09:35 +0900

    게시물 삭제 처리를 POST 방식으로 처리

--- src/main/java/com/sbs/java/jsp/board/boundedContext/global/servlet/DispatcherServlet.java
+++ src/main/java/com/sbs/java/jsp/board/boundedContext/global/servlet/DispatcherServlet.java
@@ -42,7 +42,6 @@ public class DispatcherServlet extends HttpServlet {
           case "/usr/article/list" -> articleController.showList(rq);
           case "/usr/article/detail" -> articleController.showDetail(rq);
           case "/usr/article/modify" -> articleController.showModify(rq);
-          case "/usr/article/delete" -> articleController.doDelete(rq);
           case "/usr/member/join" -> memberController.showJoin(rq);
         }
         break;
@@ -50,6 +49,7 @@ public class DispatcherServlet extends HttpServlet {
         switch (rq.getActionPath()) {
           case "/usr/article/write" -> articleController.doWrite(rq);
           case "/usr/article/modify" -> articleController.doModify(rq);
+          case "/usr/article/delete" -> articleController.doDelete(rq);
         }
         break;
     }
